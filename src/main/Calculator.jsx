621f66a25f49758160b849bfc41f3da2f3126b76
Jefferson <jefferson.leal@7comm.com.br>
2022-10-11 14:38:14 -0300

    Ajuste na lógica

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -25,30 +25,31 @@ export default class Calculator extends Component {
     }
 
     cleanMemory() {
-        this.setState({...initialState})
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
 
         if (this.state.current === 0) {
-            this.setState({ operation, current: 1, clearDisplay: true})
+            this.setState({ operation, current: 1, clearDisplay: true })
         } else {
-            const equals = operation === '=' 
+            const equals = operation === '='
+            console.log(operation)
             const currentOperation = this.state.operation
 
-            const values = [...this.states.value]
+            let values = [...this.state.values]
 
             try {
-              
+
                 values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
-            
-            }  catch(e) {
+
+            } catch (e) {
                 values[0] = this.state.values[0]
             }
 
             values[1] = 0
 
-            this.state({
+            this.setState({
                 displayValue: values[0],
                 operation: equals ? null : operation,
                 current: equals ? 0 : 1,
@@ -59,19 +60,19 @@ export default class Calculator extends Component {
     }
 
     addDigit(n) {
-        
-        if(n === '.' && this.state.displayValue.includes('.')){
-            return 
+
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
         }
 
         const clearDisplay = this.state.displayValue === '0'
-        || this.state.clearDisplay
+            || this.state.clearDisplay
 
         const currentValue = clearDisplay ? '' : this.state.displayValue
 
         const displayValue = currentValue + n
 
-        this.setState({ displayValue, clearDisplay: false})
+        this.setState({ displayValue, clearDisplay: false })
 
         if (n !== '.') {
             const i = this.state.current
@@ -79,6 +80,7 @@ export default class Calculator extends Component {
             const values = [...this.state.values]
             values[i] = newValue
             this.setState({ values })
+            console.log(values)
         }
     }
 
